e9eb62693b3600a85b5dc8b7ecaf0e8a733f4759
Luca <Lhaizx.07@gmail.com>
2025-03-17 12:20:38 +0100

    Version 3.0 + saisi

--- Program.cs
+++ Program.cs
@@ -1,18 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 
-
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-(int,int) Convert(int nbMin)
+(int, int) ConvertMinutes(int nbMin)
 {
-    int H,M;
-    M = nbMin%60;
-    H = nbMin/60;
-    return (H,M);
+    int H = nbMin / 60;
+    int M = nbMin % 60;
+    return (H, M);
 }
 
-int nbMin = 100;
-Console.WriteLine(Convert(nbMin));
+Console.WriteLine("Entrez un nombre :");
+int nbMin = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(ConvertMinutes(nbMin));
