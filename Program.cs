5f2ae89cfcd73c02eb0d0ac74fc0b53f3173e6b2
Luca <Lhaizx.07@gmail.com>
2025-03-17 12:10:43 +0100

    Version 2.0

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+(int,int) Convert(int nbMin)
+{
+    int H,M;
+    M = nbMin%60;
+    H = nbMin/60;
+    return (H,M);
+}
+
+int nbMin = 100;
+Console.WriteLine(Convert(nbMin));
